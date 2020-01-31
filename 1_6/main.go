2b3128eb0b30457f370858093986781a9c501eb9
ozank <ozan.kececioglu@nirx.de>
2020-01-31 12:51:45 +0100

    remove debug code

--- 1_6/main.go
+++ 1_6/main.go
@@ -101,24 +101,13 @@ func repeatingXor(input, key []byte) []byte {
 }
 
 func main2() {
-	inp := `Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
-	Write a function to compute the edit distance/Hamming distance between two strings. The Hamming distance is just the number of differing bits. The distance between:
-	this is a test
-	and
-	wokka wokka!!!
-	is 37. Make sure your code agrees before you proceed.
-	For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
-	The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
-	Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
-	Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
-	Solve each block as if it was single-character XOR. You already have code to do this.
-	For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.`
-	out := base64.StdEncoding.EncodeToString(repeatingXor([]byte(inp), []byte("Break repeating-key XOR")))
-	ioutil.WriteFile("6_3.txt", []byte(out), 777)
+	inp := ""
+	keyword := ""
+	out := base64.StdEncoding.EncodeToString(repeatingXor([]byte(inp), []byte(keyword)))
+	ioutil.WriteFile("6_4.txt", []byte(out), 777)
 }
 
 func main() {
-
 	dat, err := ioutil.ReadFile("6_3.txt")
 	if err != nil {
 		log.Fatal(err)
